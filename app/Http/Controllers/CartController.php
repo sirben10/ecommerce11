ebb482631e10e4a6369060079c286cfb68166d48
SirBen <akawuben@gmail.com>
2025-06-16 12:17:38 +0100

    Updating Cart Quantity

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -13,9 +13,33 @@ class CartController extends Controller
         return view('cart', compact('items'));
     }
 
+    // Add To Cart
     public function add_to_cart(Request $request)
     {
         Cart::instance('cart')->add($request->id, $request->name, $request->quantity,$request->price)->associate('App\Models\Product');
         return redirect()->back();
     }
+
+    // INcrease Cart Quatity
+    public function increase_cart_qty($rowId)
+    {
+        $product = Cart::instance('cart')->get($rowId);
+        $qty = $product->qty + 1;
+        Cart::instance('cart')->update($rowId, $qty);
+        return redirect()->back();
+    }
+    // Decrease Cart Quatity
+    public function decrease_cart_qty($rowId)
+    {
+        $product = Cart::instance('cart')->get($rowId);
+        $qty = $product->qty - 1;
+        Cart::instance('cart')->update($rowId, $qty);
+        return redirect()->back();
+    }
+    // Update To Cart
+    // public function update_cart(Request $request)
+    // {
+    //     Cart::instance('cart')->update($request->id, $request->name, $request->quantity,$request->price)->associate('App\Models\Product');
+    //     return redirect()->back();
+    // }
 }
