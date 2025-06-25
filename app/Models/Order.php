28d72d8baaad3dabb8336d9523a86e4743ee28eb
SirBen <akawuben@gmail.com>
2025-06-25 16:50:11 +0100

    Create Relationship Cardinality to the Various Models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function orderItem()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    public function transaction()
+    {
+        return $this->hasOne(Transaction::class);
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
